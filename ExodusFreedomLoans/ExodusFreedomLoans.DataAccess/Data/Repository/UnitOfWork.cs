da3fc8979fadda3b652eeda57ed425575a85db89
rebyars <rebyars@crimson.ua.edu>
2019-12-01 17:10:24 -0600

    Created Applicant Controller, created applicant.js
    Introduced the applciant controller, which allows for the submission of new applicants and see old applicants statuses.

--- ExodusFreedomLoans/ExodusFreedomLoans.DataAccess/Data/Repository/UnitOfWork.cs
+++ ExodusFreedomLoans/ExodusFreedomLoans.DataAccess/Data/Repository/UnitOfWork.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace ExodusFreedomLoans.DataAccess.Data.Repository
 {
-    class UnitOfWork : IUnitOfWork
+    public class UnitOfWork : IUnitOfWork
     {
         private readonly ApplicationDbContext _db;
 
---
+++ ExodusFreedomLoans/ExodusFreedomLoans/Areas/Partner/Controllers/ApplicantController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ExodusFreedomLoans.DataAccess.Data.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExodusFreedomLoans.Areas.Partner.Controllers
+{
+    [Area("Partner")]
+    public class ApplicantController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ApplicantController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region API Calls
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Json(new { data = _unitOfWork.Applicant.GetAll() });
+        }
+
+        #endregion 
+    }
+}
