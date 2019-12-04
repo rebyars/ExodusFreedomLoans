83f102a44cbf058d896599e87e99938ba7ba5747
rebyars <rebyars@crimson.ua.edu>
2019-12-04 00:31:53 -0600

    Videos 1-2
    Updates for vids 1, 2

---
+++ ExodusFreedomLoans/ExodusFreedomLoans.Models/ApplicationUser.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ExodusFreedomLoans.Models
+{
+    class ApplicationUser : IdentityUser
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Company { get; set; }
+
+    }
+}
---
+++ ExodusFreedomLoans/ExodusFreedomLoans.Utility/SD.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExodusFreedomLoans.Utility
+{
+    class SD
+    {
+        public const string Admin = "Admin";
+        public const string Partner = "Partner";
+    }
+}
